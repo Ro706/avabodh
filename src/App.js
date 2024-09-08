e90ee2be7cffdb91ac24cb8dc4508b5a068f167d
Ro706 <rohitmandal99091@gmail.com>
2024-09-09 01:35:00 +0530

    adding navbar element {like home about contact volunteer news}

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import NavBar from './component/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <Routes> {/* Wrap Routes */}
+          <Route path='/'/>
+          <Route path='/About'/>
+          <Route path='/Donations'/>
+          <Route path='/Volunteer'/>
+          <Route path='/News'/>
+          <Route path='/Contact'/>
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
