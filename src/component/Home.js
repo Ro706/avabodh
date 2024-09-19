7f19f83ad6fd94b9d673eacb7debbf7373140303
Ro706 <rohitmandal99091@gmail.com>
2024-09-19 11:28:02 +0530

    adding slider in home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Slider from './Slider'
 function Home() {
   return (
     <>
+      <Slider/>
+      <div>
       This is a home page
+      </div>
     </>
   )
 }
