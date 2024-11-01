30770f8d2e83c34068a355324235e0df4951a528
Ro706 <rohitmandal99091@gmail.com>
2024-11-01 12:24:11 +0530

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,53 +1,49 @@
 import React from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNavDropdown" 
-          aria-controls="navbarNavDropdown" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/features">Features</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/pricing">Pricing</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link 
-                className="nav-link dropdown-toggle" 
-                to="#" 
-                role="button" 
-                data-bs-toggle="dropdown" 
-                aria-expanded="false"
-              >
-                Dropdown link
-              </Link>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="#">Action</Link></li>
-                <li><Link className="dropdown-item" to="#">Another action</Link></li>
-                <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-              </ul>
-            </li>
-          </ul>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/"><img src="logo_avabodh.png" alt="avabodh" />Avabodh Fundation</Link>
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNavDropdown" 
+            aria-controls="navbarNavDropdown" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/About">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Donations">Donations</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Volunteer">Volunteer</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/News">News</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
